c8e314d5143427b81a417eed148a2cc49df423c0
viagra <1356971566@qq.com>
2026-01-04 19:19:10 +0800

    record类新特性

---
+++ java17-tutorial/java9-tutorial/src/main/java/org/example/basic/MoedaRecordDemo.java
@@ -0,0 +1,12 @@
+package org.example.basic;
+
+public record MoedaRecordDemo ( String base_code,
+                                String target_code,
+                                double conversion_rate,
+                                double conversion_result) {
+    @Override
+    public String toString() {
+        return String.format("Conversão de %s para %s:\nTaxa: %.4f\nValor convertido: %.2f",
+                base_code, target_code, conversion_rate, conversion_result);
+    }
+}
